4cf3477d3fa7ee119aa9b8f0dd98bb0feca60b0f
jacobkdick <jacobkdick@gmail.com>
2016-12-20 23:38:39 +0000

    Updated/added values to values state

--- pg/views/values/values.controller.js
+++ pg/views/values/values.controller.js
@@ -9,7 +9,7 @@ angular.module('acctApp')
   $scope.values = [
     {
       name: 'Obsess over customers',
-      desc: 'Providing solutions to our customers’ problems is the lifeblood of our business. And right at the essence of this excellent customer service is our relationship with each partner. We are constantly identifying market problems so that we know exactly what kinds of solutions our customers will value most. We go above and beyond by creating a positive emotional experience for our customers.',
+      desc: 'Providing solutions to our customers’ problems is the lifeblood of our business. Right at the essence of this excellent customer service is our relationship with each business partner. We will constantly identify market problems so that we know exactly what kinds of solutions our customers will value most. We go above and beyond by creating a positive emotional experience for our customers.',
     },
     {
       name: 'Be innovative & change the world',
@@ -17,7 +17,7 @@ angular.module('acctApp')
     },
     {
       name: 'Let passion & belief drive what we do',
-      desc: 'We love Mondays. And you know why? Because we’ve found our dream job—which is helping businesses fulfill their dreams. Our passion for helping businesses and our belief that it’s making a difference in their world is what drives us. When you love what you do, you’ll never work another day in your life. We really believe that.'
+      desc: 'We love Mondays. And you know why? Because we’ve found our dream job—which is helping businesses fulfill their dreams. Our passion for helping businesses and our belief that it’s making a difference in their world is what drives us.'
     },
     {
       name: 'Do what\'s right, rather than what\'s easy',
@@ -25,11 +25,15 @@ angular.module('acctApp')
     },
     {
       name: 'Stand united: All for one & one for all',
-      desc: 'To create more unity at work, we consider ourselves a family. We look out for one another, removing obstacles that might be in the way. We collaborate and listen, and we do not tolerate political games. We’re compassionate and loyal. No jobs are beneath us. When one of us succeeds, we all succeed. And when we fail, we learn from it. We stand united—and that makes us quite the dream team.'
+      desc: 'To create more unity at work, we consider ourselves a family. We look out for one another, removing obstacles that might be in the way. We collaborate and listen, and we do not tolerate political games. We’re compassionate and loyal. No jobs are beneath us. When one of us succeeds, we all succeed. And when we fail, we learn from it. We stand united.'
+    },
+    {
+      name: 'Never stop learning',
+      desc: 'Accounting and tax regulation are constantly changing and evolving. We will never be satisfied with just understanding the basics. Our commitment to our work recognizes our constant need for growth. Continuing professional education is one of the foundations of our commitment to providing the best service we can.',
     },
     {
       name: 'Maintain a quiet confidence in our abilities',
-      desc: 'As a home-grown Boise family, we are proud of our accomplishments and are confident in our ability to succeed. But we don’t want to boast about it. We believe that in the long run, our character and abilities will speak for themselves by producing a great working environment and ultimately winning the trust of our partners. And we look forward to the challenges ahead.',
+      desc: 'As a home-grown Boise family, we are proud of our accomplishments and are confident in our ability to succeed, but we don’t want to boast about it. We believe that in the long run, our character and abilities will speak for themselves by producing a great working environment and ultimately winning the trust of our partners. And we look forward to the challenges ahead.',
     },
     {
       name: 'Have a whole lot of fun',
